0d2791e63b6fab56b72e35c5c0adce2f9cf94bcf
Polina Kleiner <polina.kleiner@gmail.com>
2024-05-10 00:22:19 +0300

    feat: entities

--- src/main/java/api/entity/LoanOrder.java
+++ src/main/java/api/entity/LoanOrder.java
@@ -4,11 +4,15 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.ToString;
 
 import java.time.OffsetDateTime;
 
 @Entity
 @Table(name = "loan_order")
+@Getter
+@ToString
 public class LoanOrder {
 
     @Id
--- src/main/java/api/entity/Tariff.java
+++ src/main/java/api/entity/Tariff.java
@@ -4,9 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "tariff")
+@Getter
+@ToString
 public class Tariff {
 
     @Id
